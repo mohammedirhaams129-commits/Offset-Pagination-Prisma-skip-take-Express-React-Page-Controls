a5668b35a9f53551c7599aed9c05979f61f0ef33
mohammedirhaams129-commits <mohammed.irhaam.s.129@kalvium.community>
2026-09-21 10:34:57 +0530

    feat: pass page to threads service

--- client/src/services/threads.service.js
+++ client/src/services/threads.service.js
@@ -1,6 +1,8 @@
 import apiClient from "./apiClient";
 
-export async function getThreads() {
-  const res = await apiClient.get("/api/threads");
-  return res.data.threads;
+export async function getThreads(page) {
+  const res = await apiClient.get("/api/threads", {
+    params: { page },
+  });
+  return res.data;
 }
